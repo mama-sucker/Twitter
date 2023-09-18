0e0ac8b5986c00cd92932becccf6525049c55a6a
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2023-09-18 10:48:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Assignment 3.1 Twitter
+// By Addison Roy
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Simple Tweet length verifier\n");
+        Console.Write("Enter your message: ");
+        string message = Console.ReadLine();
+
+        if (message.Length <= 140)
+        {
+            Console.WriteLine("This Tweet can be posted (Posted)");
+        }
+        else
+        {
+            Console.WriteLine("This Tweet is too long for twitter (Rejected)");
+                goto start;
+        }
+    }
+}
+
